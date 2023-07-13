b2c7da74ad8aac7bee8895f00874e649a424afd2
Brian Lucas <97444990+brianlucas07@users.noreply.github.com>
2023-07-13 20:16:01 -0300

    Update sc.js

--- scripts/sc.js
+++ scripts/sc.js
@@ -1,5 +1,5 @@
 function abrir_whatsapp(){
-    const url = 'https://wa.me/+556199356973';
+    const url = 'https://wa.me/+5561993356973';
     window.open(url, '_blank');
     const url2 = 'https://web.whatsapp.com/send?phone=5561993356973';
     window.open(url2, '_blank');
